21e1200812db6188e2e2ed3f5f85c3e1293ece99
Luka Skugor <luka.skugor@dfinity.org>
2023-05-09 09:50:32 +0200

    Fix compile issues

--- promdump/promdump.go
+++ promdump/promdump.go
@@ -90,7 +90,7 @@ func main() {
 
 		query := fmt.Sprintf("%s[%ds]", *metric, int64(lookback))
 		log.Printf("Querying %s at %v", query, queryTS)
-		value, err, _ := api.Query(ctx, query, queryTS)
+		value, err := api.Query(ctx, query, queryTS)
                 
 		if err != nil {
 			log.Fatal(err)
